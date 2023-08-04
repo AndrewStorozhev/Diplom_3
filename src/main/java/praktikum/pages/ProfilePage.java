b7d0ca6da690027db251b1393bd53d7139e4715b
Andrey Storozhev <av.storozhev@mail.ru>
2023-08-04 21:43:10 +0400

    3. Commit

---
+++ src/main/java/praktikum/pages/ProfilePage.java
@@ -0,0 +1,22 @@
+package praktikum.pages;
+
+import io.qameta.allure.Step;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class ProfilePage {
+
+    private final WebDriver driver;
+
+    private final By nameField = By.xpath(".//input[@value='Enotiy']");
+
+    public ProfilePage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    @Step("Вернуть заголовок Имя")
+    public By getNameField() {
+        return nameField;
+    }
+
+}
